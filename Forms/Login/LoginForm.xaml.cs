630cb7bd80ee3e444c26d55870c8d5330422e424
binkul <jacekbinkul@gmail.com>
2021-04-15 16:07:47 +0200

    main DataGrid

--- Forms/Login/LoginForm.xaml.cs
+++ Forms/Login/LoginForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using LabBook.Forms.Register;
+﻿using LabBook.Forms.MainForm;
+using LabBook.Forms.Register;
 using LabBook.Security;
 using System;
 using System.Collections.Generic;
@@ -6,8 +7,6 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Input;
 
 namespace LabBook.Forms.Login
@@ -38,13 +37,30 @@ namespace LabBook.Forms.Login
                 if (connection.State == ConnectionState.Closed)
                     connection.Open();
 
-                string query = "Select Count(1) From LabBook.dbo.Users usr Where usr.login=@username and usr.password=@password";
-                SqlCommand sqlCmd = new SqlCommand(query, connection);
-                sqlCmd.CommandType = CommandType.Text;
-                sqlCmd.Parameters.AddWithValue("@username", CmbUserName.Text);
-                sqlCmd.Parameters.AddWithValue("@password", Encrypt.MD5Encrypt(TxtPassword.Password));
+                string query = "Select us.id, us.name, us.surname, us.e_mail, us.login, us.permission, us.identifier, us.active "
+                                + "From LabBook.dbo.Users us Where us.login='@username' and us.password='@password'";
+                query = query.Replace("@username", CmbUserName.Text);
+                query = query.Replace("@password", Encrypt.MD5Encrypt(TxtPassword.Password));
 
-                Login(Convert.ToInt32(sqlCmd.ExecuteScalar()));
+                SqlCommand sqlCmd = new SqlCommand(query, connection);
+                SqlDataReader reader = sqlCmd.ExecuteReader(CommandBehavior.CloseConnection);
+                User user = null;
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    var id = Convert.ToInt64(reader[0]);
+                    var name = Convert.ToString(reader[1]);
+                    var surname = Convert.ToString(reader[2]);
+                    var email = Convert.ToString(reader[3]);
+                    var login = Convert.ToString(reader[4]);
+                    var permission = Convert.ToString(reader[5]);
+                    var identifier = Convert.ToString(reader[6]);
+                    var isActive = Convert.ToBoolean(reader[7]);
+                    user = new User(id, name, surname, email, login, permission, identifier, isActive, connection);
+                }
+
+                reader.Close();
+                OpenLabBookForm(user);
             }
             catch (SqlException ex)
             {
@@ -63,11 +79,11 @@ namespace LabBook.Forms.Login
             }
         }
 
-        private void Login(int count)
+        private void OpenLabBookForm(User user)
         {
-            if (count == 1)
+            if (user != null)
             {
-                MainWindow dashboard = new MainWindow();
+                LabBookForm dashboard = new LabBookForm(user);
                 dashboard.Show();
                 this.Close();
             }
---
+++ Forms/MainForm/LabBookForm.xaml.cs
@@ -0,0 +1,41 @@
+﻿using LabBook.Security;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace LabBook.Forms.MainForm
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LabBookForm.xaml
+    /// </summary>
+    public partial class LabBookForm : Window
+    {
+        private readonly User _user;
+        private DataTable _labBookTable;
+        private DataView _labBookView;
+
+        public LabBookForm(User user)
+        {
+            InitializeComponent();
+            this._user = user;
+            SetLabBookTable();
+        }
+
+        private void SetLabBookTable()
+        {
+            string query = "Select bk.id, bk.title, bk.density, bk.remarks, bk.observation, bk.user_id, bk.cycle, " +
+                " bk.created, bk.modified From LabBook.dbo.ExpLabBook bk Order by id";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, _user.Connection);
+            _labBookTable = new DataTable();
+            adapter.Fill(_labBookTable);
+            DataColumn id = new DataColumn();
+            DataColumn[] klucz = new DataColumn[1];
+            id = _labBookTable.Columns["id"];
+            klucz[0] = id;
+            _labBookTable.PrimaryKey = klucz;
+            _labBookView = new DataView(_labBookTable);
+
+            DgLabBook.ItemsSource = _labBookView;
+        }
+    }
+}
