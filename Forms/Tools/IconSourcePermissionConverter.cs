b7200f4f8f0827a8708dd241f52a685c4ac72696
binkul <jacekbinkul@gmail.com>
2021-05-12 22:28:51 +0200

    Lock and unlock icons

--- Forms/Tools/IconSourcePermissionConverter.cs
+++ Forms/Tools/IconSourcePermissionConverter.cs
@@ -1,28 +1,41 @@
-﻿using System;
+﻿using LabBook.Security;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace LabBook.Forms.Tools
 {
     public class IconSourcePermissionConverter : IMultiValueConverter
     {
+        private const string _pathDel = "\\Img\\delete.png";
+        private const string _pathLock = "\\Img\\lock.png";
+        private const string _pathUnLock = "\\Img\\unlock.png";
+        private const string _pathOk = "\\Img\\ok.png";
+        private static ImageSource _sourceDel = new BitmapImage(new Uri(_pathDel, UriKind.Relative));
+        private static ImageSource _sourceLock = new BitmapImage(new Uri(_pathLock, UriKind.Relative));
+        private static ImageSource _sourceUnLock = new BitmapImage(new Uri(_pathUnLock, UriKind.Relative));
+        private static ImageSource _sourceOk = new BitmapImage(new Uri(_pathOk, UriKind.Relative));
+
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             var del = System.Convert.ToBoolean(values[0]);
-            //var pic1 = new BitmapImage();
-            //pic1.UriSource = new Uri("/Img/delete.png", UriKind.Relative);
-            //var pic2 = new BitmapImage();
-            //pic1.UriSource = new Uri("/Img/lock.png", UriKind.Relative);
+            var id = System.Convert.ToInt64(values[1]);
+            var user = (User)values[2];
 
             if (del)
-                return values[2];
+                return _sourceDel;
+            else if (id == user.Id)
+                return _sourceOk;
+            else if (id != user.Id && user.Permission.Equals("admin"))
+                return _sourceUnLock;
             else
-                return values[3];
+                return _sourceLock;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
