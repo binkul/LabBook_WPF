179ab59d68de61768a2912dbc992dbe4fbdae4b2
binkul <jacekbinkul@gmail.com>
2021-07-21 21:40:49 +0200

    Small changes

--- Forms/Materials/Converters/BoolToNotCollapsedConverter.cs
+++ Forms/Materials/Converters/BoolToNotCollapsedConverter.cs
@@ -4,21 +4,20 @@ using System.Windows.Data;
 
 namespace LabBook.Forms.Materials.Converters
 {
-    public class BoolToCollapsedConverter : IValueConverter
+    public class BoolToNotCollapsedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             bool tmp = System.Convert.ToBoolean(value);
             if (tmp)
-                return System.Windows.Visibility.Visible;
-            else
                 return System.Windows.Visibility.Collapsed;
+            else
+                return System.Windows.Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
-
     }
 }
